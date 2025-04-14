63410dfc284cfad52a9490debb29e581cf5ba450
Ekateryna Kravchuk <ekateryna.kravchuk@gmail.com>
2025-04-14 12:41:54 +0300

    Update DriverPage component to resolve slug parameter asynchronously and display it in the rendered output.

--- src/app/(pages)/(drivers)/driver/[slug]/page.tsx
+++ src/app/(pages)/(drivers)/driver/[slug]/page.tsx
@@ -1,4 +1,8 @@
-export default function DriverPage() {
 
-  return <div>DriverPage</div>;
+
+export default async function DriverPage({params}:{params: Promise<{slug: string}>}) {
+  const resolvedParams = await params;
+  const { slug } = resolvedParams;
+
+  return <div>DriverPage: {slug}</div>;
 }
