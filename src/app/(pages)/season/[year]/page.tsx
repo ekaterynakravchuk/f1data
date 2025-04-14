6bfe05abcd2d7cabe5f7f5b76b4b3d500c60a3ed
Ekateryna Kravchuk <ekateryna.kravchuk@gmail.com>
2025-04-14 12:48:30 +0300

    Refactor GrandPrixSeasonPage component to use async/await for resolving parameters, enhancing code clarity and ensuring proper data handling in the rendered output.

--- src/app/(pages)/season/[year]/page.tsx
+++ src/app/(pages)/season/[year]/page.tsx
@@ -1,22 +1,18 @@
-
 import GrandPrixContent from "@/features/seasons/components/GrandPrixList/grand-prix-content";
 import React from "react";
 
 type Props = {
-  params: {
+  params: Promise<{
     year: string;
-  };
+  }>;
 };
 
-const GrandPrixSeasonPage = ({ params }: Props) => {
-  const resolvedParams = React.use(Promise.resolve(params));
-
+export default async function GrandPrixSeasonPage({ params }: Props) {
+  const { year } = await params;
 
   return (
     <main className="py-6 flex flex-col gap-6">
-      <GrandPrixContent year={resolvedParams.year} />
+      <GrandPrixContent year={year} />
     </main>
   );
-};
-
-export default GrandPrixSeasonPage;
+}
