625fb559baf2467a76dd38aba10fc5149704628a
Ekateryna Kravchuk <ekateryna.kravchuk@gmail.com>
2025-04-14 12:08:48 +0300

    Enhance DriverPage component to handle missing slug parameter by returning a not found response.

--- src/app/(pages)/(drivers)/driver/[slug]/page.tsx
+++ src/app/(pages)/(drivers)/driver/[slug]/page.tsx
@@ -1,9 +1,11 @@
+import { notFound } from "next/navigation";
+
 type DriverPageProps = {
-  params: {
-    slug: string;
-  };
+  params: { slug: string };
 };
 
 export default function DriverPage({ params }: DriverPageProps) {
+  if (!params?.slug) return notFound();
+
   return <div>DriverPage: {params.slug}</div>;
 }
